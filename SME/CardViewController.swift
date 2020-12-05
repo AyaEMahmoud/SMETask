22a72f13bfed3f38158f6de20285f22e6e96291c
ayaemahmoud <ayae.mahmoud@gmial.com>
2020-12-05 09:15:56 +0200

    add main viewcontroller with xib

---
+++ SME/CardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CardViewController.swift
+//  SME
+//
+//  Created by Aya Essam on 12/5/20.
+//  Copyright © 2020 Aya E Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
